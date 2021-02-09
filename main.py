4e8f96a1b88770124b330962b92039d5e0af63d0
Rexwer123 <leorexwer@gmail.com>
2021-02-09 23:48:12 +0300

    Инициализация синтезатора речи и выбор устройства ввода
    Добавлена базовая конфигурация синтезатора речи голосового ассистента и создана функция для формирования ответов пользователю, а также добавлен выбор желаемого устройства ввода
    
    Co-Authored-By: peregrifalc <78805919+peregrifalc@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-#!/usr/bin/python
+import pyttsx3
+import speech_recognition as sr
 
-def __init__():
-	print("Hello world!")
-	exit()
+#------Начало конфигурации синтезатора речи------
+
+tts = pyttsx3.init()
+rate = tts.getProperty('rate')
+tts.setProperty('rate', rate-40)
+
+volume = tts.getProperty('volume')
+tts.setProperty('volume', volume+0.9)
+
+voices = tts.getProperty('voices')
+
+
+tts.setProperty('voice', 'ru') 
+
+for voice in voices:
+    if voice.name == 'Anna':
+        tts.setProperty('voice', voice.id)
+
+#------Конец конфигурации синтезатора речи------
+
+def saySomething(string): #Функция для синтеза речи из желаемой строки
+    tts.say(string)
+    tts.runAndWait()
+
+if __name__ == "__main__": #Выолнить код секции, если main.py был запущен как скрипт
+    for index, name in enumerate(sr.Microphone.list_microphone_names()):
+        print("Микрофон: {1}\nИндекс: {0}\n".format(index, name))
+
+    microphoneIndex = input('Индекс желаемого устройства ввода: ') #запись индекса желаемого устройства ввода
